2b3523bcc95dc09dc686c3d114ca53cb120abf3c
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-15 20:58:10 -0300

    Sumaa

---
+++ section_2/aula2/aula2.py
@@ -0,0 +1,14 @@
+'''print('\n')
+#print(123456)
+
+#print('Luiz', 'Otavio', 'Outra coisa')
+print('Luiz', 'Otávio', sep='-', end=' #### ')
+print('João','e', 'Maria', sep='-')
+
+# Print() # O python é CAP-sense'''
+
+'''
+    032.115.349-18
+'''
+print('032', '115', '239', sep='.', end='-' )
+print('18')
