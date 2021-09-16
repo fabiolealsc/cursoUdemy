587109154f1b6c577045241b2717d376fedb8de9
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-15 23:41:38 -0300

    Aulas

---
+++ section_2/aula3/aula3.py
@@ -0,0 +1,18 @@
+'''
+str - string - texto
+
+'''
+# print('alguma_coisa')
+# print("Aspas Duplas")
+# print(123456)
+
+# print("Essa é uma 'string'(str).")
+# print('Essa é uma "string"(str).')
+# print("Esse é meu \"texto\"(str).")
+# print('Esse é meu \'texto\'(str).')
+
+# print(r'Esse é meu texto \n (str)')
+# print('Esse é meu texto \n (str)')
+
+# '' "" () {} []
+
---
+++ section_2/aula4/aula4.py
@@ -0,0 +1,19 @@
+'''
+tipos de dados
+str- string texto "Assim" "12344"
+int - inteiro - 12345, -12344, 10000
+float - flutuante  1.390439, 1234,523, 3234,342 -23.342
+bool - booleano\lógico- 1 ou 0 True ou False
+'''
+#if 10 == 10:
+#    print('oi')
+
+#if 9 != 10:
+#    print('Outro oi')
+
+'''
+print(type('Luiz'))
+print(type(12234))
+print(type(1.23))
+print(type(True))
+'''
