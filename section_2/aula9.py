5eb7cbc4c33707b616bdbb8578406cc9b620bebd
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-17 02:21:35 -0300

    Organizando

---
+++ section_2/aula9.py
@@ -0,0 +1,25 @@
+'''
+    Condição IF, ELIF e ELSE
+'''
+'''
+    Operadores Relacionais
+    
+    = é igual
+    == Igual? 
+    >  Maior que
+    >= Maior ou igual a
+    <  Menor que
+    <= Menor ou igual a
+    != Diferente?
+'''
+
+'''if False:
+    print('\nVerdadeiro')
+else:
+    print('Num deu')'''
+
+'''n1 = 2
+n2 = 2
+
+exp = (n1 >= n2)
+print(exp)'''
