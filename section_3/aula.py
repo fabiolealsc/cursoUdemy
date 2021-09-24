55e58b7601094b1ed1cf1285ab134da768da683a
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-24 03:02:45 -0300

    Update aula.py

--- section_3/aula.py
+++ section_3/aula.py
@@ -1,9 +1,18 @@
 '''
     Funtion- def
 '''
-
+'''
 def funcao():
     print('Olá Mundo')
 
 for i in range(40):
-    funcao()
+    funcao()'''
+
+def a(msg='Oi', nome='Usuário'):
+    print(msg, nome)
+
+a('Ola', 'Luiz')
+a('Oi', 'Marcos')
+a('Hello', 'Pedro')
+a('Ola', 'Fabio')
+a()
