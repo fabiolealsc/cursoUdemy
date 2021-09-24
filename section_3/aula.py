806b23fd9799d6190f46f455ed2d3a5bb76e0076
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-09-24 03:19:18 -0300

    Update aula.py

--- section_3/aula.py
+++ section_3/aula.py
@@ -8,11 +8,25 @@ def funcao():
 for i in range(40):
     funcao()'''
 
-def a(msg='Oi', nome='Usuário'):
+'''def a(msg='Oi', nome='Usuário'):
+    nome, msg = nome.replace('e', '3'), msg.replace('e', '3')
+    nome, msg = nome.replace('a', '4'), msg.replace('a', '4')
+    nome, msg = nome.replace('i', '1'), msg.replace('i', '1')
+    nome, msg = nome.replace('o', '0'), msg.replace('o', '0')
     print(msg, nome)
 
-a('Ola', 'Luiz')
+a(nome='Zé', msg='Hi')
 a('Oi', 'Marcos')
 a('Hello', 'Pedro')
 a('Ola', 'Fabio')
-a()
+a()'''
+
+'''def func(var):
+    return var
+
+variavel = func('Ola mundo')
+
+if variavel:
+    print(variavel)
+else:
+    print('nada')'''
