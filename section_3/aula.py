40e4858e4aab8823fd64f3695e7813efe3d082fd
Fabio Leal Schmitz <fabio.leal.sc@gmail.com>
2021-10-06 23:27:06 -0300

    Update aula.py

--- section_3/aula.py
+++ section_3/aula.py
@@ -264,8 +264,8 @@ l1 = set(l1)
 l2 = set(l2)
 
 print(l1 == l2)'''
-
-'''l1 = [1,2,3,4,5,6,7,8,9]
+'''
+l1 = [1,2,3,4,5,6,7,8,9]
 ex1 = [variavel for variavel in l1]
 print(ex1)
 ex2 = [v * 2 for v in l1]
@@ -285,4 +285,8 @@ ex5 = dict(ex5)
 print(ex5['valor2'])
 
 l3 = list(range(100))
-print(l3)'''
+ex6 = [v for v in l3 if v % 3 == 0 if v % 8 == 0]
+print(ex6)
+ex7 = [v if v % 3 == 0 and v % 8 == 0 else "#" for v in l3]
+print(ex7)'''
+
